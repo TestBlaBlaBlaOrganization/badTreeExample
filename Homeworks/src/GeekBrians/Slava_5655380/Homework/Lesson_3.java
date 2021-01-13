b02a782b42b89d7cd0b532ebb0dba6244d5ce369
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-01-13 14:08:42 +0300

    Программа из примера переписана в ООП стиле

--- Homeworks/src/GeekBrians/Slava_5655380/Homework/Lesson_3.java
+++ Homeworks/src/GeekBrians/Slava_5655380/Homework/Lesson_3.java
@@ -1,7 +1,121 @@
 package GeekBrians.Slava_5655380.Homework;
 
+import static GeekBrians.Slava_5655380.Util.*;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Lesson_3 {
     public static void main(String[] args) {
-        System.out.println("lesson_3");
+        TicTacToeGame game = new TicTacToeGame();
+        game.start();
     }
 }
+
+class TicTacToeGame {
+    private final int SIZE;
+    private static final char DOT_EMPTY = '•';
+    private static final char DOT_X = 'X';
+    private static final char DOT_O = 'O';
+    private Scanner sc = new Scanner(System.in);
+    private Random rand = new Random();
+
+    private void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    // передалть добавив более продвинутую логику
+    private void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
+    }
+
+    private void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+
+    private boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+
+    // переделать сделав так чтобы SIZE мог быть произвольным
+    private boolean checkWin(char symb) {
+        if(map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
+        if(map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
+        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return true;
+        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) return true;
+        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) return true;
+        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return true;
+        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return true;
+        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return true;
+        return false;
+    }
+    // переделать сделав так чтобы проверялся счетчик оставшихся клеток
+    private boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+    public char[][] map;
+
+    TicTacToeGame() {
+        this(3);
+    }
+
+    TicTacToeGame(int size) {
+        SIZE = size;
+        initMap();
+
+    }
+
+    public void start() {
+        printArr(map);
+        while (true) {
+            humanTurn();
+            printArr(map);
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printArr(map);
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
+    }
+
+}
+
+
--- Homeworks/src/GeekBrians/Slava_5655380/Util.java
+++ Homeworks/src/GeekBrians/Slava_5655380/Util.java
@@ -21,11 +21,21 @@ public class Util {
         return arr;
     }
 
+
+    // Сделать так чтобы аргументом printArr мог быть массив Object произвольной глубины
+    // Спросить почему не работает конвертирование int[] в Object[] и Object в Object[]?
     public static void printArr(int[] arr) {
-        // Сделать так чтобы аргументом мог быть массив Object произвольной глубины
-        // Спросить почему не работает конвертирование int[] в Object[] и Object в Object[]?
         for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
     }
+
+    public static void printArr(char[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
+
 }
 
